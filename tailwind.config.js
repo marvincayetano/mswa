55dc23f25a4d1c92549e52af0a8913d9ab7edb56
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-07-24 15:39:59 -0400

    👍 Fix: fixing production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './layout/**/*.{js,ts,jsx,tsx}',
+        './helpers/**/*.{js,ts,jsx,tsx}',
+        // Add more here
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
